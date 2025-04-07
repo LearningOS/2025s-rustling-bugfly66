bf278f3590478dbd16c1269de6e9ce0c627cb688
bugfly66 <bugfly66@qq.com>
2025-04-07 13:17:42 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+use std::num::ParseIntError;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,10 +42,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let v = s.split(",").collect::<Vec<&str>>();
+        let name = v[0].to_string();
+        if name.len() == 0 {
+            return Person::default();
+        }
+        if v.len() != 2 {
+            return Person::default();
+        }
+        let age = v[1].parse::<usize>();
+        match age {
+            Ok(x) => return Person { name, age: x },
+            Err(_) => {
+                return Person::default();
+            }
+        }
     }
 }
 
